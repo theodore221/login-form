9a8d73959002b2f61a356f0833a7fb798fa10b7b
Theodore Xavier <theodore.xavier221@gmail.com>
2024-03-20 00:07:16 +1100

    Add css styling

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,5 @@
-import './login.css';
+import "./login.css";
+import { FaUser, FaLock } from "react-icons/fa";
 
 export default function Login() {
   return (
@@ -8,15 +9,17 @@ export default function Login() {
           <h1>Login</h1>
           <div className="input-box">
             <input type="text" placeholder="Username" required />
+            <FaUser className="icon" />
           </div>
           <div className="input-box">
             <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
           </div>
           <div className="remember-forget">
             <label>
               <input type="checkbox" /> Remember me
             </label>
-            <a href="#">Forgot Password</a>
+            <a href="#">Forgot password?</a>
           </div>
           <button type="submit" className="btn">
             Login
